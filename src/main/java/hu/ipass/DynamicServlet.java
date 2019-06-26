85781611d3264ab1bb838ea4b7b9438f7513cd69
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-26 18:13:36 +0200

    Add files via upload

--- src/main/java/hu/ipass/DynamicServlet.java
+++ src/main/java/hu/ipass/DynamicServlet.java
@@ -41,13 +41,3 @@ public class DynamicServlet extends HttpServlet {
 
 	}
 }
-'<p>' + persoonGegevens.naam + '</p>' +
-'<p>' + persoonGegevens.straatnaam + '</p>' +
-'<p>' + persoonGegevens.huisnummer + '</p>' +
-'<p>' + persoonGegevens.postcode + '</p>' +
-'<p>' + persoonGegevens.woonplaats + '</p>' +
-'<p>' + persoonGegevens.geboortedatum + '</p>' +
-'<p>' + persoonGegevens.geslacht + '</p>' +
-'<p>' + persoonGegevens.telefoonnummer + '</p>' +
-'<p>' + persoonGegevens.email + '</p>' +
-'<p>' + persoonGegevens.linkedin + '</p> <br>';
