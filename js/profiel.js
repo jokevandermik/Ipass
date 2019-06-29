eee58558cb3b90a494f286074550a58cb3ce0064
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-29 13:34:40 +0200

    Add files via upload

---
+++ js/profiel.js
@@ -0,0 +1,18 @@
+//koppelen aan een knop
+//fetch komt hier
+//post request
+//authentication?
+
+//als antwoord is 
+//if response.ok()
+//als antwoord is niet ok
+//else
+
+document.querySelector("#post").addEventListener("click", function () {
+    var formData = new FormData(document.querySelector("#POSTcustomerForm"));
+    var encData = new URLSearchParams(formData.entries());
+
+    fetch("restservices/WolfAndCherry/save", { method: 'POST', body: encData })
+        .then(response => response.json())
+        .then(function (myJson) { console.log(myJson); });
+});
