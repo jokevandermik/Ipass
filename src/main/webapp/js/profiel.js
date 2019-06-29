3bd0b26895561fb0b6d4d3898bdb431e4e6dfcfa
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-29 16:15:15 +0200

    Add files via upload

--- src/main/webapp/js/profiel.js
+++ src/main/webapp/js/profiel.js
@@ -11,8 +11,27 @@
 document.querySelector("#post").addEventListener("click", function () {
     var formData = new FormData(document.querySelector("#FormPost"));
     var encData = new URLSearchParams(formData.entries());
-
+    
+    let hoi = "hallo";
+    console.log(hoi)
+    
     fetch("restservices/WolfAndCherry/save", { method: 'POST', body: encData })
-        .then(response => response.json())
-        .then(function (myJson) { console.log(myJson); });
-});
+        .then(response => {
+        	console.log(response.status);
+        	if (response.ok){
+        		hoi = "response oke";
+        		console.log("hoi")
+        	}
+        	else if (response.status == 500){
+        		hoi = "status 500";
+        		console.log(hoi)
+        	}
+        	else{
+        		hoi = "de rest";
+        		console.log(hoi);
+        		console.log(response.status);
+        	}
+        });
+        //{response.json())
+        //.then(function (myJson)  { console.log(myJson); });
+	});
