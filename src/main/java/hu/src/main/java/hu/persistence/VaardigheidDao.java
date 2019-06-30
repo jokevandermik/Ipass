a6579fa7596bb1f6c960104c6f319d7c582aabfe
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-30 11:52:06 +0200

    Delete VaardigheidDao.java

--- src/main/java/hu/src/main/java/hu/persistence/VaardigheidDao.java
+++
@@ -1,9 +0,0 @@
-package hu.persistence;
-
-import hu.domain.Vaardigheid;;
-
-public interface VaardigheidDao {
-	public boolean save(Vaardigheid vaardigheid);
-	public boolean update(Vaardigheid vaardigheid);
-	public boolean delete(Vaardigheid vaardigheid);
-}
