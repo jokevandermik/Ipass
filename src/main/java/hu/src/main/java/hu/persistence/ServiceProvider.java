1a1453161d1176f2c92ef50f93f0bdd66be2fd44
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-30 11:51:57 +0200

    Delete ServiceProvider.java

--- src/main/java/hu/src/main/java/hu/persistence/ServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package hu.persistence;
-
-public class ServiceProvider {
-	private static ProfielService profielService = new ProfielService();
-	
-	public static ProfielService getProfielService() {
-		return profielService;
-	}
-}
