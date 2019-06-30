061384c48469acb7c5e04869bddd5710742253c1
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-30 14:07:00 +0200

    Add files via upload

--- src/main/webapp/js/profiel.js
+++ src/main/webapp/js/profiel.js
@@ -9,7 +9,9 @@
 //else
 
 document.querySelector("#post").addEventListener("click", function () {
- var jsid;
+ 
+	function haalIDop(){
+	var jsid;
  
 	
 	
@@ -47,6 +49,8 @@ document.querySelector("#post").addEventListener("click", function () {
 	console.log(jsid);
 	
 });
+}
+	haalIDop();
 	 // fetch(idhalen)
     // id json var maken
     // maak var boven aan undifind var
@@ -60,23 +64,23 @@ document.querySelector("#post").addEventListener("click", function () {
     console.log(encData);
     
     
-//    fetch("restservices/WolfAndCherry/save", { method: 'POST', body: encData })
-//        .then(response => {
-//        	console.log(response.status);
-//        	if (response.ok){
-//        		hoi = "response oke";
-//        		console.log("hoi")
-//        	}
-//        	else if (response.status == 500){
-//        		hoi = "status 500";
-//        		console.log(hoi)
-//        	}
-//        	else{
-//        		hoi = "de rest";
-//        		console.log(hoi);
-//        		console.log(response.status);
-//        	}
-//        });
+// fetch("restservices/WolfAndCherry/save", { method: 'POST', body: encData })
+// .then(response => {
+// console.log(response.status);
+// if (response.ok){
+// hoi = "response oke";
+// console.log("hoi")
+// }
+// else if (response.status == 500){
+// hoi = "status 500";
+// console.log(hoi)
+// }
+// else{
+// hoi = "de rest";
+// console.log(hoi);
+// console.log(response.status);
+// }
+// });
         // {response.json())
         // .then(function (myJson) { console.log(myJson); });
 	});
