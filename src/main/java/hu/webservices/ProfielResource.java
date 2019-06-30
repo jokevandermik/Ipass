149062d38d010354d8bcd3fe0b96a691de323fc0
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-30 17:01:01 +0200

    Add files via upload

--- src/main/java/hu/webservices/ProfielResource.java
+++ src/main/java/hu/webservices/ProfielResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
 import hu.domain.PersoonsGegevens;
@@ -82,50 +83,56 @@ public class ProfielResource {
 	
 	@POST
 	@Path("/save")
-//	@Consumes({ MediaType.MULTIPART_FORM_DATA })
-	public Response saveProfiel(@FormParam("id") int Id,@FormParam("naam") String nm, @FormParam("straatnaam") String strnm, @FormParam("huisnummer") int hsnr, @FormParam("postcode") String pc, @FormParam("woonplaats") String wp, @FormParam("geboortedatum") String gb, @FormParam("geslacht") String gs, @FormParam("telefoonnummer") int tfnr, @FormParam("email") String mail, @FormParam("linkedin") String lkin,
-			@FormParam("eigenschappen") String es, @FormParam("spreektalen") String st, @FormParam("jarenErvaringIT") int jeIT, @FormParam("technischeVaardigheden") String tv, @FormParam("functioneleVaardigheden") String fv, @FormParam("werkervaring") String we, @FormParam("computertalen") String ct, @FormParam("platformen") String pt, @FormParam("pakketen") String pk) {
+	public Response saveProfiel(@FormParam("naam") String nm) {
 		ProfielService profielService = ServiceProvider.getProfielService();
-		//check of de parameters in orde zijn (verplichte velden)
-		if(nm.trim().isEmpty()&& strnm.trim().isEmpty() && pc.trim().isEmpty() && wp.trim().isEmpty() &&
-				gb.trim().isEmpty() && mail.trim().isEmpty() && lkin.trim().isEmpty()) return Response.noContent().build();
-		//vul alle parameters voor de save
-		int id = Id;
-		String naam = nm.trim();
-		String straatnaam = strnm.trim();
-		int huisnummer = hsnr;
-		String postcode = pc.trim();
-		String woonplaats = wp.trim();
-		String geboortedatum = gb.trim();
-		String geslacht = gs;
-		int telefoonnummer = tfnr;
-		String email = mail.trim();
-		String linkedin = lkin.trim();
+		String naam = nm;
 		
-		String technischeVaardigheden = tv.trim();
-		String functioneleVaardigheden = fv.trim();
-		String werkervaring = we.trim();
-		String computertalen = ct.trim();
-		String platformen = pt.trim();
-		String pakketen = pk.trim();
-		
-		int idPersoonsGegevens = Id;
-		int idVaardigheid = Id;
-		String eigenschappen = es.trim();
-		String spreektalen = st.trim();
-		int jarenErvaringIT = jeIT;
-		//maakt de PersoonsGegevens obv de velden
-		PersoonsGegevens deGegevens = new PersoonsGegevens(id, naam, straatnaam, huisnummer, postcode, woonplaats, geboortedatum, geslacht, telefoonnummer, email, linkedin);
-		//maakt de Vaardigheid 
-		Vaardigheid deVaardigheid = new Vaardigheid(id, technischeVaardigheden, functioneleVaardigheden, werkervaring, computertalen, platformen, pakketen);
-		//maakt een profiel
-		Profiel hetProfiel = new Profiel(id, idPersoonsGegevens, idVaardigheid, eigenschappen, spreektalen, jarenErvaringIT);
-		//stelt de gegevens in
-		hetProfiel.setGegevens(deGegevens);
-		hetProfiel.setVaardigheden(deVaardigheid);
-		//sla het profiel op
-		if(profielService.save(hetProfiel)) return Response.ok(hetProfiel).build();
-		return Response.status(Status.NOT_MODIFIED).build();
-		
+		return Response.ok().build();
 	}
+//	@Consumes({ MediaType.MULTIPART_FORM_DATA })
+//	public Response saveProfiel(@FormParam("id") int Id,@FormParam("naam") String nm, @FormParam("straatnaam") String strnm, @FormParam("huisnummer") int hsnr, @FormParam("postcode") String pc, @FormParam("woonplaats") String wp, @FormParam("geboortedatum") String gb, @FormParam("geslacht") String gs, @FormParam("telefoonnummer") int tfnr, @FormParam("email") String mail, @FormParam("linkedin") String lkin,
+//			@FormParam("eigenschappen") String es, @FormParam("spreektalen") String st, @FormParam("jarenErvaringIT") int jeIT, @FormParam("technischeVaardigheden") String tv, @FormParam("functioneleVaardigheden") String fv, @FormParam("werkervaring") String we, @FormParam("computertalen") String ct, @FormParam("platformen") String pt, @FormParam("pakketen") String pk) {
+//		ProfielService profielService = ServiceProvider.getProfielService();
+//		//check of de parameters in orde zijn (verplichte velden)
+//		if(nm.trim().isEmpty()&& strnm.trim().isEmpty() && pc.trim().isEmpty() && wp.trim().isEmpty() &&
+//				gb.trim().isEmpty() && mail.trim().isEmpty() && lkin.trim().isEmpty()) return Response.noContent().build();
+//		//vul alle parameters voor de save
+//		int id = Id;
+//		String naam = nm.trim();
+//		String straatnaam = strnm.trim();
+//		int huisnummer = hsnr;
+//		String postcode = pc.trim();
+//		String woonplaats = wp.trim();
+//		String geboortedatum = gb.trim();
+//		String geslacht = gs;
+//		int telefoonnummer = tfnr;
+//		String email = mail.trim();
+//		String linkedin = lkin.trim();
+//		
+//		String technischeVaardigheden = tv.trim();
+//		String functioneleVaardigheden = fv.trim();
+//		String werkervaring = we.trim();
+//		String computertalen = ct.trim();
+//		String platformen = pt.trim();
+//		String pakketen = pk.trim();
+//		
+//		int idPersoonsGegevens = Id;
+//		int idVaardigheid = Id;
+//		String eigenschappen = es.trim();
+//		String spreektalen = st.trim();
+//		int jarenErvaringIT = jeIT;
+//		//maakt de PersoonsGegevens obv de velden
+//		PersoonsGegevens deGegevens = new PersoonsGegevens(id, naam, straatnaam, huisnummer, postcode, woonplaats, geboortedatum, geslacht, telefoonnummer, email, linkedin);
+//		//maakt de Vaardigheid 
+//		Vaardigheid deVaardigheid = new Vaardigheid(id, technischeVaardigheden, functioneleVaardigheden, werkervaring, computertalen, platformen, pakketen);
+//		//maakt een profiel
+//		Profiel hetProfiel = new Profiel(id, idPersoonsGegevens, idVaardigheid, eigenschappen, spreektalen, jarenErvaringIT);
+//		//stelt de gegevens in
+//		hetProfiel.setGegevens(deGegevens);
+//		hetProfiel.setVaardigheden(deVaardigheid);
+//		//sla het profiel op
+//		if(profielService.save(hetProfiel)) return Response.ok(hetProfiel).build();
+//		return Response.status(Status.NOT_MODIFIED).build();
+//		
+//	}
 }
