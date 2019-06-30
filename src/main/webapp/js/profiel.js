d15114fcdaf75114f1f2eb3d8fc71662db9fbe78
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-30 13:03:16 +0200

    Add files via upload

--- src/main/webapp/js/profiel.js
+++ src/main/webapp/js/profiel.js
@@ -41,7 +41,7 @@ document.querySelector("#post").addEventListener("click", function () {
 })
 .then((myJson) => {
 	console.log(JSON.stringify(myJson));
-	jsid = myJson.parse;
+	jsid = myJson.parse(myJson);
 	console.log(jsid);
 	jsid = jsid + 1;
 	console.log(jsid);
