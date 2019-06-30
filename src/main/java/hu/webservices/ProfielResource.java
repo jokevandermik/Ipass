826a75f1e231c452f60855f420af1ef60b8cef6d
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-30 20:02:46 +0200

    Add files via upload

--- src/main/java/hu/webservices/ProfielResource.java
+++ src/main/java/hu/webservices/ProfielResource.java
@@ -86,6 +86,8 @@ public class ProfielResource {
 			@FormParam("eigenschappen") String es, @FormParam("spreektalen") String st, @FormParam("jarenErvaringIT") int jeIT, @FormParam("technischeVaardigheden") String tv, @FormParam("functioneleVaardigheden") String fv, @FormParam("werkervaring") String we, @FormParam("computertalen") String ct, @FormParam("platformen") String pt, @FormParam("pakketen") String pk) {
 		ProfielService profielService = ServiceProvider.getProfielService();
 		
+		if(nm.isEmpty()) return Response.status(405).build();
+		
 		int id = Id;
 		String naam = nm;
 		String straatnaam = strnm;
