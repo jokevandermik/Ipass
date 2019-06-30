39b4d0dcf1c52267b92d7157bebf445b42f66c40
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-30 11:51:33 +0200

    Delete ProfielDao.java

--- src/main/java/hu/src/main/java/hu/persistence/ProfielDao.java
+++
@@ -1,9 +0,0 @@
-package hu.persistence;
-
-import hu.domain.Profiel;;
-
-public interface ProfielDao {
-	public boolean save (Profiel profiel);
-	public boolean update(Profiel profiel);
-	public boolean delete(Profiel profiel);
-}
