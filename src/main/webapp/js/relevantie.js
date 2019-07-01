eb8ee5e2e98956e054e40ef32dc7d1060ef59437
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-07-01 17:23:12 +0200

    Add files via upload

--- src/main/webapp/js/relevantie.js
+++ src/main/webapp/js/relevantie.js
@@ -3,30 +3,30 @@ document.querySelector("#post").addEventListener("click", function () {
 	var relevantie = lijst.options[lijst.selectedIndex].value;
 	console.log(relevantie)
 	
-//	let fetchOptions = {
-//			method: 'GET'
-//	};
-//	
-//	fetch('/restservices/WolfAndCherry/relevantie/' + relevantie, fetchOptions)
-//		.then((response) => {
-//			console.log(response.status);
-//			if (response.status = 200){
-//				hoi = "response oke";
-//				console.log(hoi);
-//				return response.json();
-//			}
-//			else if(response.status = 403){
-//			hoi = "status  403";
-//			console.log(hoi);
-//			
-//			}
-//			else {
-//				hoi = "de rest";
-//				console.log(hoi);
-//				console.log(response.status);
-//			}
-//		})
-//		.then((myJson) => {
-//			console.log(JSON.stringify(myJson));
-//		});
+	let fetchOptions = {
+			method: 'GET'
+	};
+	
+	fetch('/restservices/WolfAndCherry/relevantie/' + relevantie, fetchOptions)
+		.then((response) => {
+			console.log(response.status);
+			if (response.status = 200){
+				hoi = "response oke";
+				console.log(hoi);
+				return response.json();
+			}
+			else if(response.status = 403){
+			hoi = "status  403";
+			console.log(hoi);
+			
+			}
+			else {
+				hoi = "de rest";
+				console.log(hoi);
+				console.log(response.status);
+			}
+		})
+		.then((myJson) => {
+			console.log(JSON.stringify(myJson));
+		});
 })
