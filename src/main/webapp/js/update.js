07e291c8f228093138b3d9ee2c8bdbf0860f9242
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-07-01 20:43:04 +0200

    Add files via upload

--- src/main/webapp/js/update.js
+++ src/main/webapp/js/update.js
@@ -29,9 +29,31 @@ document.querySelector("#ophalen").addEventListener("click", function () {
 		})
 		.then((myJson) => {
 			console.log(JSON.stringify(myJson));
-//			json = JSON.parse(myJson[0]);
-//			console.log(json);
+
 			let input = document.querySelector('inputData');
-			input.innerHTML = "Naam: <input class=\"input\" type=\"text\" name=\"naam\" value=\"" + myJson[0].naam + "\" maxlength=\"50\"/><br>"
+			input.innerHTML = "Naam: <input class=\"input\" type=\"text\" name=\"naam\" value=\"" + myJson[0].naam + "\" maxlength=\"50\"/><br>" + 
+				"Straatnaam: <input class=\"input\" type=\"text\" name=\"straatnaam\" value=\" "+ myJson[0].straatnaam +"\" maxlength=\"50\"/><br>" + 
+				"Woonplaats: <input class=\"input\" type=\"text\" name=\"woonplaats\" value=\" "+ myJson[0].woonplaats +"\" maxlength=\"50\"/><br>" + 
+				"Geboortedatum: <input class=\"input\" type=\"date\" name=\"geboortedatum\" value=\" "+ myJson[0].geboortedatum +"\"/><br>" +
+				"Telefoonnummer: <input class=\"input\" type=\"number\" name=\"telefoonnummer\" value=\" "+ myJson[0].telefoonnummer +"\" maxlength=\"10\"/><br>" +
+				"<br>" + 
+				"Eigenschappen: <input class=\"input\" type=\"text\" name=\"eigenschappen\" value=\" "+ myJson[0].eigenschappen +"\" maxlength=\"256\"/><br>" +
+				"Spreektalen: <input class=\"input\" type=\"text\" name=\"straatnaam\" value=\" "+ myJson[0].spreektalen +"\" maxlength=\"256\"/><br>" +
+				"Jaren ervaring in IT: <input class=\"input\" type=\"number\" name=\"jaren_ervaring_it\" value=\" "+ myJson[0].jarenErvaringIT +"\" maxlength=\"5\"/><br>" +
+				"<br>" +
+				"Technische vaardigheden: <input class=\"input\" type=\"text\" name=\"technische_vaardigheden\" value=\" "+ myJson[0].technischeVaardigheden +"\" maxlength=\"256\"/><br>" +
+				"Werkervaring: <input class=\"input\" type=\"text\" name=\"werkervaring\" value=\" "+ myJson[0].werkervaring +"\" maxlength=\"256\"/><br>" +
+				"Platformen: <input class=\"input\" type=\"text\" name=\"platformen\" value=\" "+ myJson[0].platform +"\" maxlength=\"256\"/><br>" +
+				"</div>"+
+				"<div class=\"div_input_rechts\">" +
+				"Huisnummer: <input class=\"input\" type=\"number\" name=\"huisnummer\" value=\" "+ myJson[0].huisnummer +"\" maxlength=\"4\"/><br>" + 
+				"Postcode: <input class=\"input\" type=\"text\" name=\"postcode\" value=\" "+ myJson[0].postcode +"\" maxlength=\"6\"/><br>" + 
+				"Email: <input class=\"input\" type=\"text\" name=\"email\" value=\" "+ myJson[0].email +"\" maxlength=\"50\"/><br>" + 
+				"Linkedin: <input class=\"input\" type=\"text\" name=\"linkedin\" value=\" "+ myJson[0].linkedin +"\" maxlength=\"50\"/><br>" +
+				"<br><br><br><br><br><br><br><br>" +
+				"Functionele vaardigheden: <input class=\"input\" type=\"text\" name=\"functionele_vaardigheden\" value=\" "+ myJson[0].functioneleVaardigheden +"\" maxlength=\"256\"/><br>" +
+				"Computertalen: <input class=\"input\" type=\"text\" name=\"computertalen\" value=\" "+myJson[0].computertalen +"\" maxlength=\"256\"/><br>" +
+				"Pakketen: <input class=\"input\" type=\"text\" name=\"pakketen\" value=\""+myJson[0].pakketten +"\" maxlength=\"256\"/>" +
+				"<input class=\"submit\" type=\"button\" id=\"Put\" value=\"Aanpassen\"";
 		});
 })
