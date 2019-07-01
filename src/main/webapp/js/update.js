dd52dbb1e1bc2ae510c170f2dcf9679793e41e46
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-07-01 19:12:38 +0200

    Add files via upload

--- src/main/webapp/js/update.js
+++ src/main/webapp/js/update.js
@@ -1,39 +1,31 @@
-	function haalIDop(){
-	var id;
-    
- let fetchOptions = {
- method: 'GET',
- };
+document.querySelector("#ophalen").addEventListener("click", function () {
+	var idinput = document.getElementById("id").value;
+	console.log(relevantie)
 	
- fetch('/restservices/WolfAndCherry/Idhalen', fetchOptions)
- .then((response) => {
- console.log(response.status);
-	if (response.status = 200){
-		hoi = "response oke";
-		console.log(hoi);
-		return response.json();
-	}
-	else if(response.status = 403){
-	hoi = "status  403";
-	console.log(hoi);
+	let fetchOptions = {
+			method: 'GET'
+	};
 	
-	}
-	else {
-		hoi = "de rest";
-		console.log(hoi);
-		console.log(response.status);
-	}
+	fetch('/restservices/WolfAndCherry/aanpassen/' + id, fetchOptions)
+		.then((response) => {
+			console.log(response.status);
+			if (response.status = 200){
+				hoi = "response oke";
+				console.log(hoi);
+				return response.json();
+			}
+			else if(response.status = 403){
+			hoi = "status  403";
+			console.log(hoi);
+			
+			}
+			else {
+				hoi = "de rest";
+				console.log(hoi);
+				console.log(response.status);
+			}
+		})
+		.then((myJson) => {
+			console.log(JSON.stringify(myJson));
+		});
 })
-.then((myJson) => {
-	console.log(JSON.stringify(myJson));
-	id = JSON.parse(myJson[0].id);
-	console.log(id);
-	id = id + 1;
-	console.log(id);
-	
-	let input = document.getElementById('id');
-	input.innerHTML = "<input class='input' type='number' name='id' maxlength='4' value='" + id + "'/>";
-});
-}
-
-	haalIDop();
