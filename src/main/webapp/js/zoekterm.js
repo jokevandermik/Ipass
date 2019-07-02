3ed6373ef3f186f393d7e5db9d34f923c5761926
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-07-02 12:52:36 +0200

    Add files via upload

--- src/main/webapp/js/zoekterm.js
+++ src/main/webapp/js/zoekterm.js
@@ -1,4 +1,55 @@
 document.querySelector("#get").addEventListener("click", function () {
-	var zoek = document.getElementById("zoekterm").value;
+	var zoekterm = document.getElementById("zoekterm").value;
 	console.log(zoek);
+	
+	let fetchOptions = {
+			method: 'GET'
+	};
+	
+	fetch('/restservices/WolfAndCherry/zoekterm/' + zoekterm, fetchOptions)
+		.then((response) => {
+			console.log(response.status);
+			if (response.status = 200){
+				hoi = "response oke";
+				console.log(hoi);
+				return response.json();
+			}
+			else if(response.status = 403){
+			hoi = "status  403";
+			console.log(hoi);
+			
+			}
+			else {
+				hoi = "de rest";
+				console.log(hoi);
+				console.log(response.status);
+			}
+		})
+		.then((myJson) => {
+			console.log(JSON.stringify(myJson));
+			
+			let table = document.querySelector('tbody');
+			table.innerHTML = "";
+			
+			if(myJson == ""){
+				let error = document.querySelector('error');
+				error.innerHTML = "Er zijn geen profielen die overeenkomen gevonden.";
+				table.innerHTML = "";
+			}
+			
+			else{
+			
+			var i = 0;
+			for (i; i < myJson.length; i++) {
+				table.innerHTML = table.innerHTML + "<tr>" +
+				"<td>"+ myJson[i].id +"</td> <td>"+ myJson[i].naam +"</td> <td>"+ myJson[i].straatnaam +"</td>" +
+				"<td>"+ myJson[i].huisnummer +"</td> <td>"+ myJson[i].postcode +"</td> <td>"+ myJson[i].woonplaats +"</td>" +
+				"<td>"+ myJson[i].geboortedatum +"</td> <td>"+ myJson[i].geslacht +"</td> <td>"+ myJson[i].telefoonnummer +"</td>" +
+				"<td>"+ myJson[i].email +"</td> <td>"+ myJson[i].linkedin +"</td> <td>"+ myJson[i].eigenschappen +"</td>" +
+				"<td>"+ myJson[i].spreektalen +"</td> <td>"+ myJson[i].jarenErvaringIT +"</td> <td>"+ myJson[i].technischeVaardigheden +"</td>" +
+				"<td>"+ myJson[i].functioneleVaardigheden +"</td> <td>"+ myJson[i].werkervaring +"</td> <td>"+ myJson[i].computertalen +"</td>" +
+				"<td>"+ myJson[i].platform +"</td> <td>"+ myJson[i].pakketten +"</td>";
+			}
+			}
+		});
 }) 
