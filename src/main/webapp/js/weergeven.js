0b398b48cfca5bc16a639527989ace0efeda1a3b
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-07-02 10:11:53 +0200

    Add files via upload

---
+++ src/main/webapp/js/weergeven.js
@@ -0,0 +1,34 @@
+let hoi = "hallo";
+	console.log(hoi);
+	
+	function init(){
+				
+		let fetchOptions = {
+				method: 'GET'
+		};
+		
+		fetch('/restservices/WolfAndCherry/all', fetchOptions)
+			.then((response) => {
+				console.log(response.status);
+				if (response.status = 200){
+					hoi = "response oke";
+					console.log(hoi);
+					return response.json();
+				}
+				else if(response.status = 403){
+				hoi = "status  403";
+				console.log(hoi);
+				
+				}
+				else {
+					hoi = "de rest";
+					console.log(hoi);
+					console.log(response.status);
+				}
+			})
+			.then((myJson) => {
+				console.log(JSON.stringify(myJson));
+			});
+		}
+		
+		init();
